b2ee560cca2474b302385e9c0b8b4b4d563c1011
thejduman <thejduman@yahoo.com>
2022-09-29 17:20:39 -0400

    Cleaning data and an attempt at adding a day of week column

--- parse.py
+++ parse.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-dataset = pd.read_csv('..\data\log20140403.csv')
+dataset = pd.read_csv('..\data\log20140403.csv') #import the dataset for 2014-04-03
+to_drop = ['zone', 'idx', 'norefer', 'noagent', 'browser'] #remove irrelevant columns
+dataset.drop(to_drop, inplace=True, axis=1)
 
-print(dataset.info())
+date_series = pd.date_range(dataset.loc[:, 'date']).to_series
+dataset.assign(DayOfWeek = date_series.dt.dayofweek)
+
+print(dataset.head())
