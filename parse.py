149ac56a9b29d44b6c3103b716cdf646e1e51397
thejduman <68288652+thejduman@users.noreply.github.com>
2022-10-02 17:02:52 -0400

    Started working on datetimne conversion

--- parse.py
+++ parse.py
@@ -4,7 +4,13 @@ dataset = pd.read_csv('..\data\log20140403.csv') #import the dataset for 2014-04
 to_drop = ['zone', 'idx', 'norefer', 'noagent', 'browser'] #remove irrelevant columns
 dataset.drop(to_drop, inplace=True, axis=1)
 
-date_series = pd.date_range(dataset.loc[:, 'date']).to_series
-dataset.assign(DayOfWeek = date_series.dt.dayofweek)
+dataset['datetime'] = pd.to_datetime(dataset['date'] + dataset['time'], format = "%Y-%m-%d%H:%M:%S") #convert date and time to one field with correct data type
+#drop old date and time fields and reorder dataset
+to_drop = ['date', 'time']
+dataset.drop(to_drop, inplace=True, axis=1)
+dataset = dataset['ip', 'datetime', 'cik', 'accession', 'extention', 'code', 'size', 'find', 'crawler']
 
-print(dataset.head())
+#date_series = dataset.loc[:, 'datetime']
+#dataset.assign(DayOfWeek = date_series.dt.day_name)
+
+print(dataset.head)
