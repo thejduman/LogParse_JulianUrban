edcfbe3997c328e44dbb2ee396cc0c46fa98f7bc
thejduman <thejduman@yahoo.com>
2022-10-08 14:42:57 -0400

    Troubleshooting state

--- parse.py
+++ parse.py
@@ -18,26 +18,26 @@ dataset = dataset.assign(dayofweek = date_series) #add the series to the dataset
 
 #early morning
 early_morning_start = time(0, 0, 0)
-early_morning_end = time(6, 0, 0)
+early_morning_end = time(5, 59, 59)
 is_early_morning = (dataset['datetime'].dt.time > early_morning_start) & (dataset['datetime'].dt.time < early_morning_end)
 #morning
-morning_start = time(6, 0, 1)
-morning_end = time(12, 0, 0)
-is_morning = (dataset['datetime'].dt.time > morning_start) & (dataset['datetime'].dt.time < morning_end)
+#morning_start = time(6, 0, 0)
+#morning_end = time(11, 59, 59)
+#is_morning = (dataset['datetime'].dt.time > morning_start) & (dataset['datetime'].dt.time < morning_end)
 #afternoon
-afternoon_start = time(12, 0, 1)
-afternoon_end = time(18, 0, 0)
-is_afternoon = (dataset['datetime'].dt.time > afternoon_start) & (dataset['datetime'].dt.time < afternoon_end)
+#afternoon_start = time(12, 0, 0)
+#afternoon_end = time(17, 59, 59)
+#is_afternoon = (dataset['datetime'].dt.time > afternoon_start) & (dataset['datetime'].dt.time < afternoon_end)
 #evening
-evening_start = time(18, 0, 1)
-evening_end = time(23, 59, 59)
-is_evening = (dataset['datetime'].dt.time > evening_start) & (dataset['datetime'].dt.time < evening_end)
+#evening_start = time(18, 0, 0)
+#evening_end = time(23, 59, 59)
+#is_evening = (dataset['datetime'].dt.time > evening_start) & (dataset['datetime'].dt.time < evening_end)
 
 #assign data to columns
 dataset['timeofday'] = ''
 dataset.loc[is_early_morning, 'timeofday'] = 'Early Morning'
-dataset.loc[is_morning, 'timeofday'] = 'Morning'
-dataset.loc[is_afternoon, 'timeofday'] = 'Afternoon'
-dataset.loc[is_evening, 'timeofday'] = 'Evening'
+#dataset.loc[is_morning, 'timeofday'] = 'Morning'
+#dataset.loc[is_afternoon, 'timeofday'] = 'Afternoon'
+#dataset.loc[is_evening, 'timeofday'] = 'Evening'
 
-print(dataset['timeofday'])
+print(dataset.info())
