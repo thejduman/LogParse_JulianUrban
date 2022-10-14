925129245c929a21d5f2a66467682e3f88520a2e
thejduman <68288652+thejduman@users.noreply.github.com>
2022-10-14 17:44:48 -0400

    Implemented a new algorithm for the time of day column. Needs to be bugfixed.

--- datetimetest.py
+++ datetimetest.py
@@ -1,15 +1,37 @@
 import pandas as pd
 from datetime import time
 
-y = pd.Timestamp('2022-10-10T4:20')
-z = y.strftime("%H:%M:%S")
-converted = pd.to_datetime(z, "%H:%M:%S")
-print(z)
-#x = time(3, 35, 0)
-#print(x)
-night_start = time(0, 0, 0)
-night_end = time(5, 59, 59)
-is_night = z > night_start
-print(is_night)
-#is_night = (x.time > night_start) & (x.time < night_end)
-#print(is_night)
+def ts():
+    y = pd.Timestamp('2022-10-10T4:20')
+    z = y.time()
+    print(z)
+    #x = time(3, 35, 0)
+    #print(x)
+    night_start = time(0, 0, 0)
+    night_end = time(5, 59, 59)
+    #is_night = z > night_start
+    #print(is_night)
+    is_night = (z > night_start) & (z < night_end)
+    print(is_night)
+
+def seriescase():
+    seriestest = pd.date_range(start="2022-10-10 03:00", end="2022-10-11 00:00", periods=10)
+    a = seriestest.hour
+    print(a)
+    #night_start = time(0, 0, 0)
+    #night_end = time(5, 59, 59)
+    timeofday = []
+    for x in a:
+        if (x > 0) & (x < 6): #night = 00:00-06:00
+            timeofday.append("Night")
+        if (x > 6) & (x < 12): #morning = 06:00-12:00
+            timeofday.append("Morning")
+        if (x > 12) & (x < 18): #afternoon = 12:00-18:00
+            timeofday.append("Afternoon")
+        if (x > 18) & (x < 24): #night = 18:00-00:00 (24 doesn't exist)
+            timeofday.append("Evening")
+    print(timeofday)
+    timeofday_series = pd.Series(timeofday)
+    print(timeofday_series)
+
+seriescase()
--- parse.py
+++ parse.py
@@ -2,24 +2,45 @@ import pandas as pd
 import pytz
 from datetime import datetime, time
 
+print("Loading CSV file...")
 dataset = pd.read_csv('..\data\log20140403.csv') #import the dataset for 2014-04-03
+
+print("Pefroming initial data cleaning...")
 to_drop = ['zone', 'idx', 'norefer', 'noagent', 'browser'] #remove irrelevant columns
 dataset.drop(to_drop, inplace=True, axis=1)
 dataset.rename(columns={'extention':'doc'}, inplace=True) #rename column to be in line with the proper variable name as found on SEC.gov
 
+print("Reformatting datetime values...")
 dataset.insert(1, 'datetime', pd.to_datetime(dataset['date'] + dataset['time'], format = "%Y-%m-%d%H:%M:%S"), True) #convert date and time to one field with correct data type
 #drop old date and time fields and reorder dataset
 to_drop = ['date', 'time']
 dataset.drop(to_drop, inplace=True, axis=1)
 
+print("Adding days of week column...")
 date_series = dataset.loc[:, 'datetime'] #create new series containing datetime values
 date_series = date_series.dt.day_name() #find day of week for each datetime value in series
 dataset = dataset.assign(dayofweek = date_series) #add the series to the dataset
 
-#early morning
+print("Adding time of day column...")
+hours = dataset['datetime'].dt.hour
+timeofday = []
+for x in hours:
+    if (x > 0) & (x < 6): #night = 00:00-06:00
+        timeofday.append("Night")
+    elif (x > 6) & (x < 12): #morning = 06:00-12:00
+        timeofday.append("Morning")
+    elif (x > 12) & (x < 18): #afternoon = 12:00-18:00
+        timeofday.append("Afternoon")
+    elif (x > 18) & (x < 24): #night = 18:00-00:00 (24 doesn't exist)
+        timeofday.append("Evening")
+timeofday_series = pd.Series(timeofday)
+dataset['timeofday'] = timeofday_series
+
+
+#night
 #night_start = time(0, 0, 0)
 #night_end = time(5, 59, 59)
-#is_night = (dataset['datetime'].dt.time > night_start) & (dataset['datetime'].dt.time < night_end)
+#is_night = (dataset['datetime'].dt.time() > night_start) & (dataset['datetime'].dt.time() < night_end)
 #print(is_night.head())
 #morning
 #morning_start = time(6, 0, 0)
@@ -42,6 +63,9 @@ dataset = dataset.assign(dayofweek = date_series) #add the series to the dataset
 #dataset.loc[is_afternoon, 'timeofday'] = 'Afternoon'
 #dataset.loc[is_evening, 'timeofday'] = 'Evening'
 
+print("Adding file extension column...")
 dataset['extension'] = dataset['doc'].str.extract("(\.[0-9a-z]+$)") #extract file extensions from file names
 
+print("The dataset is ready.")
+print(dataset.head())
 print(dataset.tail())
